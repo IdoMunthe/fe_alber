ae59ae2be174f5e00d4825915b0e0f126cc1ecc3
Ido Munthe <idomunthe52@gmail.com>
2024-09-26 15:40:50 +0700

    Finished Process Order and Tracking History Page

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -22,7 +22,6 @@ const Dashboard = () => {
 
       <View className="bg-[#F0D800] py-2 px-32 w-full items-center gap-y-2 rounded-b-xl">
         <Text
-          // style={{ backgroundColor: "red" }}
           className="text-3xl font-bold text-white text-center w-64"
         >
           Dashboard User
