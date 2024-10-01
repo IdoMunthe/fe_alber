5a91e74bcb3a792b5386b2612d77b80d63ae80c1
Ido Munthe <idomunthe52@gmail.com>
2024-10-01 08:43:44 +0700

    Finished Setting Application Feature

--- app/setting-application.tsx
+++ app/setting-application.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import CustomHeader from "../components/CustomHeader";
+import Title from "../components/Title";
 
 const SettingApplication = () => {
-  return (
-    <View>
-      <Text>SettingApplication</Text>
-    </View>
-  )
-}
 
-export default SettingApplication
+  
+
+  return (
+    <View className="flex-1 bg-white">
+      <CustomHeader />
+      <Title title="Setting Application" />
+      <View>
+        <Image
+          source={require("../assets/images/user.jpg")}
+          className="w-20 h-20 rounded-full"
+        />
+
+      </View>
+    </View>
+  );
+};
+
+export default SettingApplication;
