fa01997fb8e93a752bf134047984964515649348
Ido Munthe <idomunthe52@gmail.com>
2024-10-09 11:33:16 +0700

    Applied Admin PCS role

--- app/(submission-tracking)/submission-tracking.tsx
+++ app/(submission-tracking)/submission-tracking.tsx
@@ -1,14 +1,24 @@
-import { View, Text, Image, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 import Title from "../../components/Title";
 import CustomHeader from "../../components/CustomHeader";
 import Green1 from "../../components/Green1";
 import Green2 from "../../components/Green2";
 import { useRouter } from "expo-router";
+import Loading from "../../components/Loading";
 
 const SubmissionTracking = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+
+  if (isLoading) return <Loading />;
 
   return (
     <>
@@ -16,16 +26,26 @@ const SubmissionTracking = () => {
       <SafeAreaView style={styles.container}>
         <Title title="Submission Tracking" />
         <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={() => router.push('/process-order')}>
-          <Image source={require("../../assets/images/process-order.png")} />
+          <TouchableOpacity
+            onPress={() => {
+              router.push("/process-order");
+              setIsLoading(false);
+            }}
+          >
+            <Image source={require("../../assets/images/process-order.png")} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push('/history-order')}>
-          <Image source={require("../../assets/images/history-order.png")} />
+          <TouchableOpacity
+            onPress={() => {
+              router.push("/history-order");
+              setIsLoading(false);
+            }}
+          >
+            <Image source={require("../../assets/images/history-order.png")} />
           </TouchableOpacity>
         </View>
       </SafeAreaView>
-      <Green1 customStyle={{zIndex: 0}}/>
-      <Green2 customStyle={{zIndex: 1}}/>
+      <Green1 customStyle={{ zIndex: 0 }} />
+      <Green2 customStyle={{ zIndex: 1 }} />
     </>
   );
 };
